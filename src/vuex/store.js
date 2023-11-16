6a9b27dff4ef3063eb63df59b96d4466d8ff5c4e
Priyanshu Arya <priyanshuarya22@gmail.com>
2023-11-16 15:05:13 +0530

    first commit

---
+++ src/vuex/store.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import Vue from "vue"
+import Vuex from 'vuex'
+import getters from './getters'
+import actions from './actions'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+const state = {
+    shoppinglists: []
+}
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    getters,
+    actions
+})
